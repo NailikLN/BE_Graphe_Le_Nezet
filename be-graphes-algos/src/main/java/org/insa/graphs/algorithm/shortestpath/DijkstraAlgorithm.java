a0ac8b8e25db938234e86526979302ff77bc4b15
NailikLN <lenezetkilian@gmail.com>
2021-04-12 12:50:52 +0200

    final implementation of dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,9 +1,12 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-import java.util.ArrayList;
 
-import org.insa.graphs.model.Graph;
-import org.insa.graphs.model.Node;
+import java.util.*;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.*;
+
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 	
@@ -18,15 +21,102 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
         
-        ArrayList<Label> Labels = null;
+        /* initialisation des tableaux et varaiable dont on auras besoin*/
+        
     	Graph graph = data.getGraph();
-    	for(int i = 0; i < graph.size(); i++)
-        {
-        	 Labels.add(new Label(graph.getNodes().get(i), false, (Integer)null));
-        }
+    	Node Origin = data.getOrigin();
+    	Node Destination = data.getDestination();
+    	int SizeGraph = graph.size();
+    	
+    	Label Labels[] = new Label[SizeGraph];
+
+    	
+    	List<Node> ToTest = graph.getNodes();
+    	
+    	for(Node node: ToTest)
+    	{
+    		Labels[node.getId()] = new Label(node);
+    	}
+
+    	BinaryHeap<Label> LabelHeap = new BinaryHeap<Label>();
+    	
+    	/* Initialisation avec le point d'origine */
+    	
+    	
+    	Labels[Origin.getId()].setCost(0);
+    	LabelHeap.insert(Labels[Origin.getId()]);
+    	
+    	Boolean IsFinished = false;
+
+    	
+    	while(!LabelHeap.isEmpty() && !IsFinished)
+    	{
+    		
+    		Label CurrentNode = LabelHeap.findMin();
+    		LabelHeap.remove(CurrentNode);
+    		CurrentNode.setMark(true);
+    		
+    		// on vérifie que on ne se trouve pas a la fin.
+    		
+    		if(CurrentNode.getNode() == Destination)
+    		{
+    			IsFinished = true;
+    			continue;
+    			
+    		}
+    		
+    		
+    		for(Arc arc : CurrentNode.getNode().getSuccessors())
+    		{
+    			
+    			if (!data.isAllowed(arc)) 
+    			{
+                    continue;
+                }
+    			
+    			Label SuccNode = Labels[arc.getDestination().getId()];
+    			
+    			if(!SuccNode.isMarked())
+    			{
+    				if(SuccNode.getCost() > (CurrentNode.getCost() + arc.getLength()))
+    				{
+    					SuccNode.setCost((CurrentNode.getCost()+arc.getLength()));
+    					SuccNode.setFather(CurrentNode.getNode(), arc);
+    					LabelHeap.insert(SuccNode);
+    					
+    				}
+    			}
+    			
+    		}
+    		
+    		// pour savoir si on arrive a la fin du programme on test si la destination a un predecesseur (father)
+    		
+    		
+    		
+    	}
+    	if(Labels[Destination.getId()].getFather() == null)
+		{
+			solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+		} 
+		else 
+		{
+			ArrayList<Arc> arcs = new ArrayList<Arc>();
+			Arc ArcFromfather = Labels[Destination.getId()].getFatherArc();
+			while(ArcFromfather != null)
+			{
+				arcs.add(ArcFromfather);
+				ArcFromfather = Labels[ArcFromfather.getOrigin().getId()].getFatherArc();
+			}
+			
+			Collections.reverse(arcs);
+			
+			solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+		}
+    	
 
     	
         return solution;
     }
-
+    
+    
 }
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -3,26 +3,32 @@ package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.model.*;
 
 
-public class Label{
+public class Label implements Comparable<Label>{
 	
-	private boolean marque = false;
+	private boolean marque;
+	private float cout;
+	private Node node;
+	private Node father;
+	private Arc fatherArc;
 	
-	private int cout = (Integer)null;
 	
-	private Node node = null;
-	
-	
-	public Label(Node node, boolean marque,int cout)
+	public Label(Node node)
 	{
 		this.node = node;
-		this.marque = marque;
-		this.cout = cout;
+		this.marque = false;
+		this.cout = Float.POSITIVE_INFINITY;
+		this.father = null;
 		
 	}
 	
-	public int getCost()
+	public float getCost()
 	{
 		return this.cout;
+	}
+	
+	public void setCost(float cost)
+	{
+		this.cout = cost;
 	}
 	
 	public boolean isMarked()
@@ -30,8 +36,50 @@ public class Label{
 		return this.marque;
 	}
 	
+	public void setMark(boolean marque)
+	{
+		this.marque = marque;
+	}
+	
 	public Node getNode()
 	{
 		return this.node;
 	}
+	
+	public Node getFather()
+	{
+		return this.father;
+	}
+	
+	public Arc getFatherArc()
+	{
+		return this.fatherArc;
+	}
+
+	@Override
+	public int compareTo(Label o) 
+	{
+		int result;
+		if(this.getCost() < o.getCost())
+		{
+			result = -1;
+		}
+		else if(this.getCost() > o.getCost())
+		{
+			result = 1;
+		}
+		else
+		{
+			result = 0;
+		}
+		
+		return result;
+	}
+
+	public void setFather(Node node2, Arc arc) {
+		this.father = node2;
+		this.fatherArc = arc;
+	}
+
+
 }
